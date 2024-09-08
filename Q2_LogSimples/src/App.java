757705d764c1edeaecf5e541c205c4b19011c847
Thomaz <141247521+thomazabrantes@users.noreply.github.com>
2024-09-08 20:56:49 +0000

    branch nova com exercicio 2

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+        LogSimples log = LogSimples.getInstance();
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -1,22 +1,45 @@
-import java.time.LocalDate;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+// Todos os clientes devem acessar uma única instância compartilhada da classe 
+// Nenhuma instância adicional deve ser criada acidentalmente
 
-public class LogSimples implements Iterable<String>{
-    private List<String> mensagens;
+/*
+ * Solução? 
+ * Alternativa 1: Classe com todos os métodos e atributos estáticos
+ * Alternativa 2: Classe definida com um contrutor privado que cria uma instância única de si mesma;
+ *                Fornece-se um método estático que retorna uma referência para a instância única.
+ */
 
-    public LogSimples(){
-        mensagens = new LinkedList<>();
-    }
-
-    public void log(String m){
-        String logM = LocalDate.now().toString() + " : " + m;
-        mensagens.add(logM);
-    }
-
-    @Override
-    public Iterator<String> iterator() {
-        return mensagens.iterator();
-    }
-}
+ import java.time.LocalDate;
+ import java.util.Iterator;
+ import java.util.LinkedList;
+ import java.util.List;
+ 
+ public class LogSimples implements Iterable<String>{
+     private List<String> mensagens;
+     static LogSimples instance;
+ 
+     private LogSimples(){
+         mensagens = new LinkedList<>();
+     }
+ 
+     static synchronized public LogSimples getInstance() {
+         if(instance == null)
+             instance = new LogSimples();
+         return (instance);
+     }
+ 
+     @Override
+     public Object clone() throws CloneNotSupportedException {
+         throw new CloneNotSupportedException();
+     }
+ 
+     public void log(String m){
+         String logM = LocalDate.now().toString() + " : " + m;
+         mensagens.add(logM);
+     }
+ 
+     @Override
+     public Iterator<String> iterator() {
+         return mensagens.iterator();
+     }
+ }
+ 
